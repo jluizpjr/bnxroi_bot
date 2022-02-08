4c1c747fe67deda9f30b03ed9039eac7caf9935f
Joao Luiz Pereira <jluizpjr@gmail.com>
2022-02-08 15:05:14 -0300

    Flask v0

---
+++ flask-app/app.py
@@ -0,0 +1,17 @@
+import sqlite3
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+def get_db_connection():
+    conn = sqlite3.connect('../ama-1001796235673.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    questions = conn.execute('SELECT * FROM ama').fetchall()
+    conn.close()
+    return render_template('index.html', questions=questions)
