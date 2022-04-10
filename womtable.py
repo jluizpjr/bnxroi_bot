7735d089b16c0122e79b57bb280d4d06486c41d2
Joao <jluizpjr@gmail.com>
2022-04-10 18:53:09 -0300

    Saving memory

--- womtable.py
+++ womtable.py
@@ -21,5 +21,6 @@ def womtable():
     plt.ylabel('Hora (Brasil)', fontsize = 12) # y-axis label with fontsize 15
     plt.show()
     plt.savefig('table.png')
+    del df, hm, sns, plt
     
     return
